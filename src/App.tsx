4cc615c4012de639277d72ef67a926fba93bee30
Amr Tamer Elharery <amrelharery@gmail.com>
2026-02-10 00:03:06 +0200

    Enable Dark Mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
 import { useState } from 'react'
 import './App.css'
 import { Button } from './components/ui/button';
+import { ThemeProvider, useTheme } from './lib/theme-provider';
 
-function App() {
-  const [count, setCount] = useState(0)
+function ThemeControls() {
+  const { theme, setTheme } = useTheme();
 
   return (
     <>
-      <div className="flex min-h-svh flex-col items-center justify-center">
+      <p>Current {theme}</p>
+      <div className="flex gap-3">
+        <Button variant={'outline'} onClick={() => setTheme('light')}>Light Theme</Button>
+        <Button onClick={() => setTheme('dark')}>Dark Theme</Button>
+        <Button onClick={() => setTheme('system')}>System Theme</Button>
+      </div>
+    </>
+  );
+}
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="zad-theme">
+      <div className="flex min-h-svh flex-col items-center justify-center gap-3 bg-background text-primary">
         <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <ThemeControls />
       </div>
-
-    </>
+    </ThemeProvider>
   );
 }
 
